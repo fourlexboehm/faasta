395c928b4f11dd90f1a63ba175680459f160dd03
fourlexboehm <kavsuy4jw1qyqzfv2524jyt6@systemli.org>
2025-04-21 08:56:53 +0000

    Deploying to gh-pages from @ fourlexboehm/faasta@f865221b282ebad504cb92114abab2880c01f6e2 🚀

--- trait.impl/faasta_interface/trait.FunctionService.js
+++ trait.impl/faasta_interface/trait.FunctionService.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["faasta_interface",[]],["server_wasi",[["impl FunctionService for <a class=\"struct\" href=\"server_wasi/rpc_service/struct.FunctionServiceImpl.html\" title=\"struct server_wasi::rpc_service::FunctionServiceImpl\">FunctionServiceImpl</a>"]]]]);
+    var implementors = Object.fromEntries([["faasta_interface",[]],["server_wasi",[["impl <a class=\"trait\" href=\"faasta_interface/trait.FunctionService.html\" title=\"trait faasta_interface::FunctionService\">FunctionService</a> for <a class=\"struct\" href=\"server_wasi/rpc_service/struct.FunctionServiceImpl.html\" title=\"struct server_wasi::rpc_service::FunctionServiceImpl\">FunctionServiceImpl</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[23,219]}
+//{"start":57,"fragment_lengths":[23,345]}
