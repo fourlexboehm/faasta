cd25150104d56d4fd692172b198031d9deb4bbcb
fourlexboehm <kavsuy4jw1qyqzfv2524jyt6@systemli.org>
2025-04-27 03:21:40 +0000

    Deploying to gh-pages from @ fourlexboehm/faasta@30049232eac9ddc01e2e818938f76adfc78c6507 🚀

--- server_wasi/sidebar-items.js
+++ server_wasi/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["load_tls_config","main","redirect_to_website","run_server","text_response"],"mod":["cert_manager","github_auth","metrics","rpc_service"],"static":["SERVER"],"struct":["Args","FaastaClientState","FaastaServer"]};
+window.SIDEBAR_ITEMS = {"fn":["load_tls_config","main","redirect_to_website","run_http_redirect_server","run_server","text_response"],"mod":["cert_manager","github_auth","metrics","rpc_service"],"static":["SERVER"],"struct":["Args","FaastaClientState","FaastaServer"]};
