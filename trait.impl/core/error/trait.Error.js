ee45475ff7a3a48e91f962c253429fcd3e12fc0d
fourlexboehm <kavsuy4jw1qyqzfv2524jyt6@systemli.org>
2025-05-19 11:02:58 +0000

    Deploying to gh-pages from @ fourlexboehm/faasta@6bbfd5924430d8c69b2335ac283a75833a23cfcf 🚀

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["faasta_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"faasta_interface/enum.FunctionError.html\" title=\"enum faasta_interface::FunctionError\">FunctionError</a>"]]]]);
+    var implementors = Object.fromEntries([["faasta_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"faasta_interface/enum.FunctionError.html\" title=\"enum faasta_interface::FunctionError\">FunctionError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
